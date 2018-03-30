5a5b29cbf9b32c29b00e46143a0c66cf3ad7cfc6
LigntShot <32431934+LigntShot@users.noreply.github.com>
2018-03-30 19:59:25 +0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,37 +1,41 @@
-#include <avr/io.h>
-#include <avr/common.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
+#include <avr/io.h> 
+#include <avr/common.h> 
+#include <avr/interrupt.h> 
+#include <util/delay.h> 
 
-#define COM1			PD7
-#define	COM2			PD5
-#define COM3			PD4
-#define COM4			PB3
+#define ENGINE_LEFT PD7 
+#define ENGINE_RIGHT PD5 
+#define COM3 PD4 
+#define COM4 PB3 
 
-#define COM1_DIR		DDRD
-#define COM2_DIR		DDRD
-#define COM3_DIR		DDRD
-#define COM4_DIR		DDRB
+#define ENGINE_DIR DDRD 
+#define COM2_DIR DDRD 
+#define COM3_DIR DDRD 
+#define COM4_DIR DDRB 
 
-#define COM1_PORT		PORTD
-#define COM2_PORT		PORTD
-#define COM3_PORT		PORTD
-#define COM4_PORT		PORTB
+#define ENGINE_PORT PORTD 
+#define COM2_PORT PORTD 
+#define COM3_PORT PORTD 
+#define COM4_PORT PORTB 
 
-#define COM1_PIN		PIND
-#define COM2_PIN		PIND
-#define COM3_PIN		PIND
-#define COM4_PIN		PINB
+/*#define ENGINE_PIN PIND 
+#define COM2_PIN PIND 
+#define COM3_PIN PIND 
+#define COM4_PIN PINB 
+#define COM5_PIN PINA */ 
 
-volatile uint8_t x = 0;
+volatile uint8_t x = 0; 
+volatile uint8_t y = 0; 
+volatile uint8_t Z = 0; 
 
-void
-configure_pins()
-{
-	COM1_DIR |= _BV(COM1);
+void 
+configure_pins() 
+{ 
+	ENGINE_DIR |= _BV(ENGINE_LEFT) | _BV(ENGINE_RIGHT); 
+	ENGINE_PORT &= ~(_BV(ENGINE_LEFT) | _BV(ENGINE_RIGHT)); 
 
-	DDRC &= ~(_BV(PC3));
-	PORTC |= _BV(PC3);
+	DDRA = 0x00; 
+	PORTA = 0xFF; 
 }
 
 void
