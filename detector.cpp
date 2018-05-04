4838db635f01849e7b00125f44c6f103c5a25d28
LigntShot <densh23@gmail.com>
2018-05-04 18:46:39 +0500

    Добавлены фотопрерыватели

--- detector.cpp
+++ detector.cpp
@@ -30,6 +30,11 @@
 
 #define INPUT_PIN PINA
 
+#define pi 3.14
+#define desired_speed 30
+#define k 1
+#define dt 1
+
 volatile uint8_t cmd = 0;
 volatile bool left_wh = 1;
 volatile bool right_wh = 1;
@@ -40,6 +45,16 @@ volatile uint8_t cnt2 = 0;
 volatile bool left_engine_run = false;
 volatile bool right_engine_run = false;
 
+volatile uint8_t speed_cnt_left = 0;
+volatile uint8_t speed_cnt_right = 0;
+volatile uint8_t timer_cnt = 0;
+
+volatile uint8_t speed_left = 0;
+volatile uint8_t speed_right = 0;
+
+volatile uint8_t pwm_left = 0;
+volatile uint8_t pwm_right = 0;
+
 void
 configure_pins()
 {
@@ -48,7 +63,6 @@ configure_pins()
 
 	DETECTOR_PORT &= ~(_BV(COM_DETECTOR_OUTPUT_LEFT) | _BV(COM_DETECTOR_OUTPUT_RIGHT)); //ÇÀÏÈÑÛÂÀÅÌ Â ÏÎÐÒ ÍÓËÈ
 	//DETECTOR_PIN &= ~(_BV(COM_DETECTOR_INPUT)); //ÇÀÏÈÛÂÀÅÌ Â ÏÈÍ ÍÎËÜ
-
 	ENGINE_DIR |= _BV(ENGINE_LEFT) | _BV(ENGINE_RIGHT);
 	ENGINE_PORT &= ~(_BV(ENGINE_LEFT) | _BV(ENGINE_RIGHT));
 }
@@ -60,38 +74,63 @@ configure_timer()
 	TIMSK |= _BV(TOIE0);
 }
 
-int
-counter() //ÔÓÍÊÖÈß ÎÁÍÓËÅÍÈß Ñ×¨Ò×ÈÊÀ
+bool
+counter(uint8_t target, uint8_t max = 100) //ÔÓÍÊÖÈß ÎÁÍÓËÅÍÈß Ñ×¨Ò×ÈÊÀ
 {
-	cnt0++;
+	/*cnt0++;
 	if (cnt0 == 10) cnt0 = 0;
-	return cnt0;
+	return cnt0;*/
+
+	if (cnt0 > max) cnt0 = 0;
+	return cnt0++ > target;
 }
 
-int
-delay_counter() //ÔÓÍÊÖÈß ÎÁÍÓËÅÍÈß Ñ×¨Ò×ÈÊÀ
+bool
+delay_counter(uint8_t target, uint8_t max = 100) //ÔÓÍÊÖÈß ÎÁÍÓËÅÍÈß Ñ×¨Ò×ÈÊÀ
 {
-	cnt2++;
+	/*cnt2++;
 	if (cnt2 == 92) cnt2 = 0;
-	return cnt2;
+	return cnt2;*/
+
+	if (cnt2 > max) cnt2 = 0;
+	return cnt2++ > target;
 }
 
-int
-pwm_counter() {
-	cnt1++;
-	if (cnt1 == 20) cnt1 = 0;
-	return cnt1;
+bool
+pwm_counter(uint8_t target, uint8_t max = 100)
+{
+	if (cnt1 > max) cnt1 = 0;
+	return cnt1++ > target;
+}
+
+ISR(INT0_vect)
+{
+	speed_cnt_left++;
+}
+
+ISR(INT1_vect)
+{
+	speed_cnt_right++;
 }
 
 ISR(TIMER0_OVF_vect)
 {
+	speed_left = speed_cnt_left * dt;
+	speed_right = speed_cnt_right * dt;
+
+	speed_cnt_left = 0;
+	speed_cnt_right = 0;
+
+	pwm_left  += k * (desired_speed - speed_left);
+	pwm_right += k * (desired_speed - speed_right);
+
 	switch(cmd) {
 	case 0: //ÂÊËÞ×ÅÍÈÅ ËÅÂÎÃÎ ÄÀÒ×ÈÊÀ
 		DETECTOR_PORT |= _BV(COM_DETECTOR_OUTPUT_LEFT);
 		cmd = 1;
 		break;
 	case 1: //ÍÈ×ÅÃÎ ÍÅ ÄÅËÀÒÜ
-		if (counter() > 8) cmd = 2;
+		if (counter(8)) cmd = 2;
 		break;
 	case 2: // Ñ×ÈÒÛÂÀÅÍÈÅ ÈÇ ËÅÂÎÃÎ ÄÀÒ×ÈÊÀ
 		if (DETECTOR_PIN & _BV(COM_DETECTOR_INPUT)) right_wh = true;//ENGINE_PORT |= _BV(ENGINE_RIGHT)
@@ -108,7 +147,7 @@ ISR(TIMER0_OVF_vect)
 		cmd = 5;
 		break;
 	case 5:
-		if(counter() > 8) cmd = 6;
+		if(counter(8)) cmd = 6;
 		break;
 	case 6:
 		if (DETECTOR_PIN & _BV(COM_DETECTOR_INPUT)) left_wh = true;
@@ -129,18 +168,22 @@ ISR(TIMER0_OVF_vect)
 			right_engine_run = true;
 			//ENGINE_PORT |= _BV(ENGINE_RIGHT);
 		}
-		if(delay_counter() > 30) {
+		if(delay_counter(pwm_left)) {
 			cmd = 0;
-			right_wh = false;
 			left_wh = false;
 		}
-
+		if (delay_counter(pwm_right))
+		{
+			cmd = 0;
+			right_wh = false;
+		}
 	}
-	if (left_engine_run & (pwm_counter() < 5)) ENGINE_PORT |= _BV(ENGINE_LEFT);
+	if (left_engine_run & pwm_counter(pwm_left)) ENGINE_PORT |= _BV(ENGINE_LEFT);
 	else ENGINE_PORT &= ~_BV(ENGINE_LEFT);
 
-	if (right_engine_run & (pwm_counter() < 5)) ENGINE_PORT |= _BV(ENGINE_RIGHT);
+	if (right_engine_run & pwm_counter(pwm_right)) ENGINE_PORT |= _BV(ENGINE_RIGHT);
 	else ENGINE_PORT &= ~_BV(ENGINE_RIGHT);
+
 }
 
 int
